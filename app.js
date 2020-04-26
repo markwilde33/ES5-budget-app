228fdf110b7bab4b00884ec074752f1983f00822
Mark Wilde <markwilde29@gmail.com>
2020-04-26 14:03:27 +0100

    Set up our app modules using iife and closures, so as to keep data private. Methods can be used to return specific data to the public.

---
+++ app.js
@@ -0,0 +1,22 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+   //code
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function () {
+
+   //code
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+   //code
+
+})(budgetController, UIController);
