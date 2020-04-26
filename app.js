4e58bbcf22060fc1f3daa7081c14d75a4c182b7f
Mark Wilde <markwilde29@gmail.com>
2020-04-26 16:18:23 +0100

    Implemented a more efficent way to store query strings, allows for changing class names in the future, to be more straightforward.

--- app.js
+++ app.js
@@ -9,14 +9,25 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
+   var DOMstrings = {
+      inputType: '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputBtn: '.add__btn'
+   }
+
    return {
       getInput: function () {
 
          return {
-            type: document.querySelector('.add__type').value, // can be either inc or exp
-            description: document.querySelector('.add__description').value, // get description
-            value: document.querySelector('.add__value').value // get value
+            type: document.querySelector(DOMstrings.inputType).value, // can be either income or expense
+            description: document.querySelector(DOMstrings.inputDescription).value, // get description of the input
+            value: document.querySelector(DOMstrings.inputValue).value // get monetary value
          };
+      },
+
+      getDOMstrings: function () {
+         return DOMstrings;
       }
    };
 
@@ -25,6 +36,8 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+
+   var DOM = UICtrl.getDOMstrings();
 
    var ctrlAddItem = function () {
 
@@ -36,10 +49,9 @@ var controller = (function (budgetCtrl, UICtrl) {
       // 3. Add the item to the UI
       // 4. Calculate the budget
       // 5. Display the budget on the UI
-      console.log('testing!');
    }
 
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+   document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
    document.addEventListener('keypress', function (event) {
 
