68e1a7102bf63dc06754b43b6f5c3b0440b6ac21
Mark Wilde <markwilde29@gmail.com>
2020-04-28 11:23:19 +0100

    Added logic to calculate budget and % of income spent.

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ var budgetController = (function () {
       totals: {
          exp: 0,
          inc: 0
-      }
+      },
+      budget: 0,
+      percentage: -1
+
    };
 
    return {
@@ -62,8 +65,23 @@ var budgetController = (function () {
          calculateTotal('exp');
          calculateTotal('inc');
          // calculate the budget: income - expenses
+         data.budget = data.totals.inc - data.totals.exp;
 
          // calculate percentage of income spent
+         if (data.totals.inc > 0) {
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+         } else {
+            data.percentage = -1;
+         }
+      },
+
+      getBudget: function () {
+         return {
+            budget: data.budget,
+            totalInc: data.totals.inc,
+            totalExp: data.totals.exp,
+            percentage: data.percentage
+         };
       },
 
       testing: function () {
@@ -149,10 +167,13 @@ var controller = (function (budgetCtrl, UICtrl) {
 
    var updateBudget = function () {
       // 1. Calculate the budget
+      budgetCtrl.calculateBudget();
 
-      // 2. return the budget
+      // 2. Return the budget
+      var budget = budgetCtrl.getBudget();
 
       // 3. Display the budget on the UI
+      console.log(budget);
    };
 
    var ctrlAddItem = function () {
